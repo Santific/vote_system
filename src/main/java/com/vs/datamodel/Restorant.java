bfc98a7dd1da140f626b958037a53d744b4fbd7a
Santific <yavorsky.yuriy@gmail.com>
2015-11-14 16:08:09 +0100

    fix

--- src/main/java/com/vs/datamodel/Restorant.java
+++ src/main/java/com/vs/datamodel/Restorant.java
@@ -29,7 +29,7 @@ public class Restorant implements Serializable {
 		this.name = name;
 	}
 
-	@OneToMany(mappedBy="dishKey.restorantName", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@OneToMany(mappedBy="dishKey.restorantName", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
 	public Collection<Dish> getDishes() {
 		return dishes;
 	}
@@ -38,7 +38,7 @@ public class Restorant implements Serializable {
 		this.dishes = dishes;
 	}
 	
-	@OneToMany(mappedBy="restorantName", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@OneToMany(mappedBy="restorantName", fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
 	public Collection<Vote> getVotes() {
 		return votes;
 	}
